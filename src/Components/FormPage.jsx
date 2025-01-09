8248b8f76a0f7f37ff68b44c6863a2ba26d27868
Karthika <karthikakg2903@gmail.com>
2025-01-08 22:32:05 -0800

    second commit

--- src/Components/FormPage.jsx
+++ src/Components/FormPage.jsx
@@ -15,18 +15,20 @@ const FormPage = () => {
      
       <form className="form-content">
         <div className='twoPart'>
-          <div className='logo'>
-          <img src={Logo} />
+          <div className='logo-center'>
+          <img src={Logo}/>
           </div>
-          <div>
-        <input type="text" placeholder="Price List" className="form-input" />
-        <input type="text" placeholder="25/4/2025 to 30/4/2025" className="form-input" />
-          </div>
-      <div className="form-logo">
+      
+        {/* <input type="text" placeholder="Price List" className="form-input" /> */}
+        {/* <input type="text" placeholder="25/4/2025 to 30/4/2025" className="form-input" /> */}
+        <h2 style={{marginTop:"-10px"}}>Price list</h2>
+        <h3 style={{marginTop:"-8px"}}>25:04:2025 to 29:04:2025</h3>
+
+      {/* <div className="form-logo">
         <h1>
           FAC<span style={{ color: "#007BFF" }}>IO</span>
         </h1>
-      </div>
+      </div> */}
         <input type="text" placeholder="Shop Name" className="form-input" />
         <input type="text" placeholder="Mobile Number" className="form-input phone" />
         <button
--- src/Components/ShopList.jsx
+++ src/Components/ShopList.jsx
@@ -1,27 +1,29 @@
+
 import React, { useState } from 'react';
 import Img1 from '../assets/pic2.jpg';
-import Img3 from '../assets/pic3.jpg';
-import Img4 from '../assets/pic4.jpg';
-import Img2 from '../assets/pic5.png';
-import Img6 from '../assets/pic6.jpg';
-import Img7 from '../assets/pic1.jpg';
-import Img8 from '../assets/pic7.jpg';
-import Img9 from '../assets/pic10.jpg';
-import Img10 from '../assets/pic9.jpg';
+import Img2 from '../assets/pic3.jpg';
+import Img3 from '../assets/pic4.jpg';
+import Img4 from '../assets/pic5.png';
+import Img5 from '../assets/pic6.jpg';
+import Img6 from '../assets/pic1.jpg';
+import Img7 from '../assets/pic7.jpg';
+import Img8 from '../assets/pic10.jpg';
+import Img9 from '../assets/pic9.jpg';
 
 
 
 const ShopList = () => {
   const [shops, setShops] = useState([
-    { id: 1, name: "100 PLUS CAN", count: 50, price: "24*320ML", checked: false, image: Img1 },
-    { id: 2, name: "100 PLUS", count: 50, price: "24*500ML", checked: false, image: Img3},
-    { id: 3, name: "100 PLUS", count: 50, price: "12*1.5L", checked: false, image:Img4 },
-    { id: 4, name: "COCA COLA", count: 50, price: "12*1.5L", checked: false, image: Img2},
-    { id: 5, name: "COCO COLA CAN", count: 50, price: "24*320ML", checked: false, image:Img6 },
-    { id: 6, name: "DASANI DRINKING WATER", count: 50, price: "24*600ML", checked: false, image: Img7 },
-    { id: 7, name: "DASANI DRINKING WATER", count: 50, price: "12*1.5L", checked: false, image: Img8 },
-    { id: 8, name: "DUTCH LADY CHOCOLATE", count: 50, price: "24*200ML", checked: false, image: Img9 },
-    { id: 9, name: "DUTCH LADY CHOCOLATE", count: 50, price: "12*1L", checked: false, image: Img10 },
+    { id: 1, name: "100 PLUS CAN", count: "24*320ML", price: 100, checked: false, image: Img1 },
+    { id: 2, name: "100 PLUS", count: "24*500ML", price: 100, checked: false, image: Img2 },
+    { id: 3, name: "100 PLUS", count: "12*1.5L", price: 100, checked: false, image: Img3 },
+    { id: 4, name: "COCO COLA", count: "12*1.5L", price: 100, checked: false, image: Img4 },
+    { id: 5, name: "COCO COLA CAN", count: "24*320ML", price: 100, checked: false, image: Img5 },
+    { id: 6, name: "DASANI DRINKING WATER", count: "24*600ML", price: 100, checked: false, image: Img6 },
+    { id: 7, name: "DASANI DRINKING WATER", count: "12*1.5L", price: 100, checked: false, image: Img7 },
+    { id: 8, name: "DUTCH LADY CHOCOLATE", count: "24*200ML", price: 100, checked: false, image: Img8 },
+    { id: 9, name: "DUTCH LADY CHOCOLATE", count: "12*1L", price: 100, checked: false, image: Img9 },
+
 
   ]);
 
@@ -34,7 +36,7 @@ const ShopList = () => {
   };
 
   return (
-    <div style={{ padding: "10px", boxSizing: "border-box",  backgroundColor:"#ddd"}}>
+    <div style={{ padding: "10px", boxSizing: "border-box" }}>
       {shops.map((shop) => (
         <div
           key={shop.id}
@@ -60,14 +62,17 @@ const ShopList = () => {
             }}
           />
           <div style={{ flex: 1 }}>
-            <h3 style={{ margin: 0, fontSize: "1rem" }}>{shop.name}</h3>
-            <p style={{ margin: "5px 0", color: "gray", fontSize: "0.9rem" }}>
-              {shop.count} Shop available
+            <h3 style={{ margin: 0, fontSize: "1rem", }}>{shop.name}</h3>
+            <p style={{ margin: "5px 0", fontSize: "0.9rem", fontFamily: "Poppins",fontWeight:"bold" }}>
+              Size {shop.count}
             </p>
-            <p style={{ margin: 0, fontWeight: "bold", fontSize: "1rem" }}>
-              Size {shop.price}
-            </p>
+            {/* <p style={{ margin: 0, fontWeight: "bold", fontSize: "1rem", fontFamily:"Poppins"}}>
+              RM {shop.price}
+            </p> */}
           </div>
+          <p style={{ margin: "5px", fontWeight: "bold", fontSize: "1rem", fontFamily: "Poppins"  }}>
+            RM {shop.price}
+          </p>
           <div
             onClick={() => handleCheckboxClick(shop.id)}
             style={{
@@ -78,12 +83,14 @@ const ShopList = () => {
               justifyContent: "center",
               alignItems: "center",
               cursor: "pointer",
-              backgroundColor: shop.checked ? "#0800ff" : "#E0E0E0",
+              backgroundColor: shop.checked ? "#007BFF" : "#E0E0E0",
               transition: "background-color 0.3s",
             }}
           >
+
             {shop.checked && <span style={{ color: "#fff", fontSize: "16px" }}>✔</span>}
           </div>
+
         </div>
       ))}
       <button
